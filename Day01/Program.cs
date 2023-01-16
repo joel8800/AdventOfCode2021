233f3bd788e57abc08f2e749cfd713ef8e7a83c4
Joel Kaneshiro <joel8800@gmail.com>
2023-01-15 17:08:36 -0800

    Completed Day01, added AoCUtils library project

--- Day01/Program.cs
+++ Day01/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AoCUtils;
+
+Console.WriteLine("Day01: Sonar Sweep");
+
+string[] input = FileUtil.ReadFileByLine("input.txt");
+
+int prevMeas = 0;
+int increases = -1;         // first increase doesn't count
+
+foreach (string line in input)
+{
+    int currMeas = Convert.ToInt32(line);
+
+    if (currMeas > prevMeas)
+        increases++;
+
+    prevMeas = currMeas;
+}
+
+Console.WriteLine($"Part1: {increases}");
+
+int meas1 = 0;
+int meas2 = 0;
+int last3ptSum = 0;
+
+increases = -1;             // first increase doesn't count
+
+foreach (string line in input)
+{
+    int currMeas = Convert.ToInt32(line);
+
+    if (meas1 == 0)         // this is the first measurement
+    {
+        meas1 = currMeas;
+        continue;
+    }
+
+    if (meas2 == 0)         // this is the second measurement
+    {
+        meas2 = currMeas;
+        continue;
+    }
+
+    // subsequent measurements use the previous two to create a 3 pt sum
+    int curr3ptSum = meas1 + meas2 + currMeas;
+
+    // compare the current 3pt sum to the previous
+    if (last3ptSum < curr3ptSum)
+        increases++;
+
+    last3ptSum = curr3ptSum;
+
+    // slide the measurements over, getting ready for next iteration
+    meas1 = meas2;
+    meas2 = currMeas;
+}
+
+Console.WriteLine($"Part2: {increases}");
